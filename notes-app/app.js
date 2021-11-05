142ad4a3b73ee2b46ad442e042712e105596e861
oguzzsancaktar <oguztahasancaktar@gmail.com>
2021-11-05 19:49:47 +0300

    first

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,12 @@
-const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'This file created from NodeJS')
+const getNotes = require('./notes.js')
+const validator = require('validator')
+
+const msg = getNotes();
+const isEmail = validator.isEmail('asdfas@gmail.com')
+
+console.log(msg);
+
+console.log(isEmail);
+
+
