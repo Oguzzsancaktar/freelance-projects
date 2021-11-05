4e83835be4925d62a270cc280870343c9ad4859c
oguzzsancaktar <oguztahasancaktar@gmail.com>
2021-11-05 20:43:01 +0300

    ok

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,11 +2,12 @@
 const getNotes = require('./notes.js')
 const validator = require('validator')
 
+
+
 const msg = getNotes();
 const isEmail = validator.isEmail('asdfas@gmail.com')
 
 console.log(msg);
-
-console.log(isEmail);
+console.log(validator);
 
 
