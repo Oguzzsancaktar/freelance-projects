61387980df6f54b34a72d02fc94c2fa5c8912d54
oguzzsancaktar <oguztahasancaktar@gmail.com>
2021-11-11 19:30:29 +0300

    part7 ok

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,36 +1,73 @@
 const path = require('path');
 const express = require('express');
-
-
+const hbs = require('hbs');
 
 const app = express();
 
+// Define paths for Express Config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '/../templates/partials');
 
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-  res.send('Home Page')
-})
-
-// app.get('/help', (req, res) => {
-//   res.send('<h1>Help</h1>')
-// })
-
-// app.get('/about', (req, res) => {
-//   res.send('<h1>About Page</h1>');
-
-// });
-
-app.get('/weather', (req, res) => {
-  res.send({
-    location: "Philadelphia",
-    forecast:"Weather Forecast"
+  res.render('index', {
+    title: 'Weather title',
+    name: 'Weather name',
+    message: 'Weather message',
   });
 });
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help title',
+    name: 'help name',
+    message: 'help message',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('About', {
+    title: 'About title',
+    name: 'About name',
+    message: 'About message',
+  });
+});
+
+
+app.get('/about/*', (req, res) => {
+  res.render('404', {
+    title: '404 about',
+    name: '404 about name',
+    errorMessage: 'About article not found',
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404 help',
+    name: '404 help name',
+    errorMessage: 'Help article not found',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: '404 name',
+    errorMessage: 'Page not found',
+  });
+})
 
 
 app.listen(3000, () => {
   console.log('App running on port 3000');
   console.log('http://localhost:3000');
-})
+});
