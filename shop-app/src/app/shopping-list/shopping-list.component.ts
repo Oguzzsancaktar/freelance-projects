2d7efd65ec1ca3e82198dec1ea2fa5cef908ff9c
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-01-18 18:53:50 +0300

    half

---
+++ shop-app/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'shopping-list',
+  templateUrl: './shop-list.component.html'
+})
+
+export class ShoppingListComponent {
+  
+}
