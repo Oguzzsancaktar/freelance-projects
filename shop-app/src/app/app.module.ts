df8e14b9bb00c4e0ce0b62ec799d67d651b18512
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-01-20 16:06:31 +0300

    part 5 OK

--- shop-app/src/app/app.module.ts
+++ shop-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -14,6 +15,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     AppComponent,
     HeaderComponent,
     RecipesComponent,
+    RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
     ShoppingEditComponent,
--- shop-app/src/app/shopping-list/shopping-list.component.ts
+++ shop-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Ingredient } from "src/shared/ingredient.model";
 
 @Component({
-  selector: 'shopping-list',
+  selector: 'app-shopping-list',
   templateUrl: './shop-list.component.html'
 })
 
-export class ShoppingListComponent {
-  
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatoes  ',10),
+  ];
+
+  ngOnInit() {
+
+  }
 }
