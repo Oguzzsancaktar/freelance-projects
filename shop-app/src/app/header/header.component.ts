17c9e9811545940b027a9da40fb503a067333107
oguzzsancaktar <oguztahasancaktar@gmail.com>
2022-01-20 18:27:24 +0300

    6 - 1 half

--- shop-app/src/app/header/header.component.ts
+++ shop-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,12 @@ import { Component } from "@angular/core";
 })
 
 export class HeaderComponent{
+  @Output() onSwitchTab: EventEmitter<{name:string}> = new EventEmitter();
 
+
+  switchTab(tabName){
+    this.onSwitchTab.emit({
+      name:tabName
+    })
+  }
 }
